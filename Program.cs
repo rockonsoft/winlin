9394b84727a021f94734eacdee70afd7292921b8
hano <hanobotha@rockonsoft.com>
2017-01-17 18:20:40 +0200

    added source files

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace winlin
+{
+    internal class Program
+    {
+        public async static void Work()
+        {
+            Console.WriteLine($"Thread Id in Work:{Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine("Start doing work");
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            Console.WriteLine("Work done");
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine($"Thread Id in main:{Thread.CurrentThread.ManagedThreadId}");
+            Work();
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# winlin
+# Rockonsoft Blog
+Welcome to the source samples for the [Rockonsoft blog](http://rockonsoft.blogspot.co.za/).
+
+## A comparison of the IL generated for Mono and .NET for async methods
+[Blog](http://rockonsoft.blogspot.co.za/2017/01/source-code-asyncawait-keywords-was.html)
